250623cffd6fef64a1b67eb715203fd2e75c517c
souvikk27 <souvikvip@outlook.com>
2022-12-18 20:13:19 +0530

    Setting up the Middleware

--- CakeShop/CakeShop/Program.cs
+++ CakeShop/CakeShop/Program.cs
@@ -5,9 +5,19 @@ namespace CakeShop
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseStaticFiles();
+
+            if(app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.MapDefaultControllerRoute();
 
             app.Run();
         }
